2f4da4292caf56a56d395409fa6b334dfdb0a492
Lalit Kumar <Lalit Kumar@host.docker.internal>
2022-12-24 02:23:47 +0530

    This is my changed

--- src/com/lait/practical/HelloWorld.java
+++ src/com/lait/practical/HelloWorld.java
@@ -3,6 +3,10 @@ package com.lait.practical;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
+		int x=10;
+		int y=20;
+		
+		System.out.println("Total sum is = "+x+y);
 	}
 
 }
